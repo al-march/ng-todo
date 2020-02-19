0fa2db4d8b8b542cb240ee46b9188756da375ea1
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2020-02-19 15:26:10 +0300

    с

---
+++ n/src/app/todos.ts
@@ -0,0 +1,17 @@
+export const todos = [
+  {
+    title: 'Phone XL',
+    done: false,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    title: 'Phone Mini',
+    done: false,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    title: 'Phone Standard',
+    done: false,
+    description: ''
+  }
+];
